c737e1ab346a86b23fc811427e7b507593d386bd
Giovanny Salcedo <gsalcedo26@gmail.com>
2024-01-29 18:54:53 -0400

    [crud_of_banks][test] some tests were fixed because they were inconsistent when creating and searching

--- spec/system/banks_spec.rb
+++ spec/system/banks_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'CRUD de bancos', type: :system do
-  it 'permite crear un nuevo banco' do
+  it 'crea un nuevo banco con éxito' do
     visit new_bank_path
 
     fill_in 'Nombre', with: 'Banco Ejemplo'
     click_button 'Guardar'
 
-    expect(page.find('.alert')).to have_content('Banco creado correctamente')
+    expect(Bank.last).to be
   end
 
   it 'rechaza un banco con un nombre de más de 50 caracteres' do
@@ -16,33 +16,44 @@ RSpec.describe 'CRUD de bancos', type: :system do
     fill_in 'Nombre', with: 'Banco con un nombre que excede los 50 caracteres' * 2
     click_button 'Guardar'
 
-    expect(page).to have_content('Nombre es demasiado largo (máximo 50 caracteres)')
+    expect(Bank.where(name: 'Banco con un nombre que excede los 50 caracteres' * 2)).to be_empty
   end
 
   it 'muestra la lista de bancos' do
-    create(:bank, name: 'Banco A')
-
+    visit new_bank_path
+  
+    fill_in 'Nombre', with: 'Banco A'
+    click_button 'Guardar'
     visit banks_path
 
     expect(page).to have_content('Banco A')
   end
 
   it 'permite actualizar un banco existente' do
-    bank = create(:bank, name: 'Banco Original')
+    visit new_bank_path
 
-    visit edit_bank_path(bank)
+    fill_in 'Nombre', with: 'Banco Ejemplo'
+    click_button 'Guardar'
+    sleep 2
+    visit bank_path(Bank.last)
+    visit edit_bank_path(Bank.last)
 
-    fill_in 'Nombre', with: 'Banco Modificado'
-    click_button 'Guardar cambios'
+    fill_in 'Nombre', with: 'Banco Actualizado'
+    click_button 'Guardar'
 
-    expect(page).to have_content('Banco actualizado correctamente')
+    # Comprobamos que el nombre se ha actualizado correctamente
+    expect(page).to have_content('Banco Actualizado')
   end
 
   it 'permite eliminar un banco existente' do
-    bank = create(:bank, name: 'Banco a Eliminar')
+    visit new_bank_path
 
-    visit banks_path
-    click_link 'Eliminar', href: bank_path(bank)
+    fill_in 'Nombre', with: 'Banco Ejemplo'
+    click_button 'Guardar'
+    sleep 2
+    visit bank_path(Bank.last)
+
+    click_button 'Eliminar'
 
     expect(page).to have_content('Banco eliminado correctamente')
   end
