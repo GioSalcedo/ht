6a0b7a7999b411b4e3670fdf1ac5ae1e4af94c2c
Giovanny Salcedo <gsalcedo26@gmail.com>
2024-02-04 23:44:54 -0400

    [dashboard][components][routes] added routa for lookbook

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,16 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-  root to: "home#index"
+  # Defines the root path route ('/')
+  # root 'posts#index'
+  root to: 'home#index'
+  get 'dashboard' => 'dashboard#dashboard', as: :dashboard
   resources :banks
   resources :suppliers
 
   if Rails.env.development?
-    mount Lookbook::Engine, at: "/lookbook"
+    mount Lookbook::Engine, at: '/lookbook'
   end
-
 end
